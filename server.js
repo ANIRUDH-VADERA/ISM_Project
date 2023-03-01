d80f380393d822ee6b3e03da5a05f863f989af66
dwivedi-ayush <79781363+dwivedi-ayush@users.noreply.github.com>
2023-03-01 14:58:47 +0530

    updated server.js
    now different devices under same LAN can also connect

--- server.js
+++ server.js
@@ -3,26 +3,39 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.use(express.static(__dirname + '/public'));
+// Enable CORS
+app.use((req, res, next) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (request, response) {
-    response.sendFile(__dirname + "/public/home.html");
+  response.sendFile(__dirname + "/public/home.html");
 });
 
 // Checking if the users connect
 io.on("connection", (socket) => {
-    console.log("A user connected");
-    // Checking if the users disconnect
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
-    socket.on("message", (message) => {
-        console.log("message", message);
-        // Broadcasting this message to all the users that are connected
-        io.emit("message", message);
-    });
+  console.log("A user connected");
+  // Checking if the users disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+  socket.on("message", (message) => {
+    console.log("message", message);
+    // Broadcasting this message to all the users that are connected
+    io.emit("message", message);
+  });
 });
 
-http.listen(5500, function () {
-    console.log("server start");
+const hostname = "localhost"; // change this to your LAN IP address
+const port = 5500;
+http.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
